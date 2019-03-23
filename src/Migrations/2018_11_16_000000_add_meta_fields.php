7ea3a957d3f44923b7f522c53f94cb1516531ea0
Adam Huang <adamhut@gmail.com>
2019-03-23 10:41:01 -0700

    Update 2018_11_16_000000_add_meta_fields.php

--- src/Migrations/2018_11_16_000000_add_meta_fields.php
+++ src/Migrations/2018_11_16_000000_add_meta_fields.php
@@ -13,21 +13,38 @@ class AddMetaFields extends Migration
      */
     public function up()
     {
-        Schema::table('wink_tags', function (Blueprint $table) {
-            $table->text('meta')->nullable();
-        });
 
-        Schema::table('wink_pages', function (Blueprint $table) {
-            $table->text('meta')->nullable();
-        });
+        if (Schema::hasTable('wink_tags')) {
+            Schema::table('wink_tags', function (Blueprint $table) {
+                if (!Schema::hasColumn('wink_tags', 'meta')) {
+                    $table->text('meta')->nullable();
+                }
+            });
+        }
 
-        Schema::table('wink_authors', function (Blueprint $table) {
-            $table->text('meta')->nullable();
-        });
+        if (Schema::hasTable('wink_pages')) {
+            Schema::table('wink_pages', function (Blueprint $table) {
+                if (!Schema::hasColumn('wink_pages', 'meta')) {
+                    $table->text('meta')->nullable();
+                }
+            });
+        }
 
-        Schema::table('wink_posts', function (Blueprint $table) {
-            $table->text('meta')->nullable();
-        });
+        if (Schema::hasTable('wink_authors')) {
+            Schema::table('wink_authors', function (Blueprint $table) {
+                if (!Schema::hasColumn('wink_authors', 'meta')) {
+                    $table->text('meta')->nullable();
+                }
+            });
+        }
+
+        if (Schema::hasTable('wink_posts')) {
+            Schema::table('wink_posts', function (Blueprint $table) {
+                if (!Schema::hasColumn('wink_posts', 'meta')) {
+                    $table->text('meta')->nullable();
+                }
+            });
+        }
 
         if (Schema::hasTable( 'wink_categories')) {
             Schema::table( 'wink_categories', function (Blueprint $table) {
@@ -45,9 +62,12 @@ class AddMetaFields extends Migration
      */
     public function down()
     {
+
+
         Schema::table('wink_tags', function (Blueprint $table) {
             $table->dropColumn('meta');
         });
+
 
         Schema::table('wink_pages', function (Blueprint $table) {
             $table->dropColumn('meta');
